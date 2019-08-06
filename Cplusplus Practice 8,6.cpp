c1e91870ffc323b794eb55adddf4f99d6b643ef2
Arlene Waghalter <43083580+toastandjams@users.noreply.github.com>
2019-08-06 13:30:07 -0500

    Add files via upload

---
+++ Cplusplus Practice 8,6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello\n";
+	std::cout << "Goodbye\n";
+	std::cout << "This is a practice\n";
+	std::cout << "I'm focusing on some c++ development\n";
+	//this interface is a pain - about to change preferences
+	//much better
+	
+	std::cout << "We have the display to console down,\n";
+	std::cout << "let's try something more advanced:\n";
+	
+	int score;//practice from code academy - declare a variable
+	
+	score = 0;//initialize the variable by assigning 0 to it
+	
+}
